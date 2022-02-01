892813dede5cf087534f8fa3313e86eeac47b137
Kyle <kyle4346@gmail.com>
2022-02-01 15:54:04 -0600

    changed about page into phases page

---
+++ frontend/src/views/Phases.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Phases">
+    <h1>Payment Phases Sheet</h1>
+    <HelloWorld/>
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from "../components/HelloWorld.vue"
+export default {
+  name: 'Phases',
+  components: {
+    HelloWorld
+  }
+}
+</script>
