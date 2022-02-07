ffd1622b091a49b2afc2e745be6c30a482f0f5f6
Kyle <kyle4346@gmail.com>
2022-02-07 03:11:39 -0600

    added components and db.json file for fake backend just in case

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Phases from '../views/Phases.vue'
 
 const routes = [
   {
@@ -10,10 +11,7 @@ const routes = [
   {
     path: '/Phases',
     name: 'Phases',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Phases.vue')
+    component: Phases
   }
 ]
 
