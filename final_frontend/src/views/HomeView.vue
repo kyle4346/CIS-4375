b6aa1522730cb85cfc62dd64b8c19f87be3f3a2f
SALVAZELA1023 <60159878+SALVAZELA1023@users.noreply.github.com>
2022-04-07 22:46:44 -0500

    Updated views
    Added slide images

--- final_frontend/src/views/HomeView.vue
+++ final_frontend/src/views/HomeView.vue
@@ -5,8 +5,8 @@ HelloWorld componenent to produce a view when clicking on CFC Mananagement APP--
     <HelloWorld style="text-align: center" msg="Welcome to Pipeline International" />
     <!-- <img src="https://i.pinimg.com/originals/3e/6b/0b/3e6b0b4cd20e10744886b5653c3ee9b9.jpg"  class="center" > -->
 
-    <img src="../assets/const.jpg" class = "image">
-    <!-- <Slider /> -->
+    <!-- <img src="../assets/const.jpg" class = "image"> -->
+    <Slider />
 
   </div>
 </template>
@@ -17,13 +17,13 @@ HelloWorld componenent to produce a view when clicking on CFC Mananagement APP--
 // @ is an alias to /src
 //Calling the component HelloWorld create vue instance
 import HelloWorld from "@/components/HelloWorld.vue";
-// import Slider from "./SliderComponent.vue";
+import Slider from "./SliderComponent.vue";
 
 export default {
   name: "myHome",
   components: {
     HelloWorld,
-    // Slider,
+    Slider,
   },
   
 };
@@ -47,13 +47,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #000000;
-  font-weight: bold;
+  color: #2c3e50;
   
 }
 
-.image {
-  max-width: 70%;
-  max-height: 50%;
+#image {
+  max-width: 52.39px;/* 70*/
+  max-height: 50px;/* 50*/
+}
+
+#image3 {
+  max-width: 48.75px;/* 70*/
+  max-height: 50px;/* 50*/
+}
+
+#image2 {
+  max-width: 150px;
+  max-height: 50px;
 }
 </style>
