f272886cb9d8926aac7d11423c92d08eca46c36e
kyle4346 <kyle4346@gmail.com>
2022-04-11 18:55:13 -0500

    working slide gallery

--- front-backup/src/views/HomeView.vue
+++ front-backup/src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <template>
   <!-- Inside the temaplate adding some text and calling the 
 HelloWorld componenent to produce a view when clicking on CFC Mananagement APP-->
-  <div id="home"  > <!-- :style="{'background-image':'url(https://hips.hearstapps.com/pop.h-cdn.co/assets/15/22/1600x2179/gallery-1432675837-1-choose.jpg?resize=980:*)'}"-->
-    <HelloWorld style="text-align: center" msg="Welcome to Pipeline International" />
+  <div id="home">
+    <!-- :style="{'background-image':'url(https://hips.hearstapps.com/pop.h-cdn.co/assets/15/22/1600x2179/gallery-1432675837-1-choose.jpg?resize=980:*)'}"-->
+    <HelloWorld
+      style="text-align: center"
+      msg="Welcome to Piping International"
+    />
     <!-- <img src="../assets/Add.png"  class="center" > -->
 
     <!-- <img src="../assets/logo1.jpeg" class = "image"> -->
-    <button id = "toTop" @click="moveUp()"> </button>
-    
+
+    <img class="slide-image" :src="getImgUrl(images[0])" />
+
+    <button id="toTop" @click="moveUp()"></button>
+
     <!-- <Slider /> -->
-
-    
-
   </div>
 </template>
 
@@ -25,16 +29,35 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "myHome",
+  data() {
+    return {
+      images: ["team.jpeg", "const.jpg","project.jpeg","project1.jpeg","project2.jpeg"], //these images are located inside of assets folder
+    };
+  },
   components: {
     HelloWorld,
     // Slider,
   },
+  watch: {
+    getImgUrl() {},
+  },
+  mounted() {
+    window.setInterval(() => {
+      this.slide();
+    }, 5000);
+  },
   methods: {
-    moveUp(){
-           window.scrollTo(0,0);
-      }
-  }
-  
+    moveUp() {
+      window.scrollTo(0, 0);
+    },
+    getImgUrl(im) {
+      return require("../assets/" + im);
+    },
+    slide() {
+      let first = this.images.shift();
+      this.images = this.images.concat(first);
+    },
+  },
 };
 </script>
 
@@ -57,17 +80,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  
 }
 
 #image {
-  max-width: 52.39px;/* 70*/
-  max-height: 50px;/* 50*/
+  max-width: 52.39px; /* 70*/
+  max-height: 50px; /* 50*/
 }
 
 #image3 {
-  max-width: 48.75px;/* 70*/
-  max-height: 50px;/* 50*/
+  max-width: 48.75px; /* 70*/
+  max-height: 50px; /* 50*/
 }
 
 #image2 {
@@ -77,11 +99,15 @@ export default {
 #toTop {
   position: fixed;
   border: 1px solid blue;
-  background-color: rgba(255,200,200,0.5);  
+  background-color: rgba(255, 200, 200, 0.5);
   width: 300px;
   height: 100px;
   bottom: 0;
   left: 0;
   right: 0;
+}
+.slide-image {
+  height:600px;
+  width:70%
 }
 </style>
